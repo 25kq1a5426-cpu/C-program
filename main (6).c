fe0d9b530be4d1acd4ceda4d465a021b52842b6a
25kq1a5426-cpu <25kq1a5426@pace.ac.in>
2025-11-27 11:35:06 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int i=1;
+ void main()
+{
+    int printnum();
+    printnum();
+    printf("final i value is %d",i);
+}
+int printnum()
+{
+    printf(" %d",i++);
+    if(i==6)
+    return 0;
+    printnum();
+}
+
