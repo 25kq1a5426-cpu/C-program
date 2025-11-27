cf3ed75441b62fdd345c64873051e16feff1630e
25kq1a5426-cpu <25kq1a5426@pace.ac.in>
2025-11-27 10:56:12 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ void main()
+{
+    int lower,upper;
+    void printnum(int,int);
+    scanf("%d%d",&lower,&upper);
+    printnum(lower,upper);
+}
+void printnum(int lower,int upper)
+{
+    printf("%d ",lower);
+    lower++;
+    if(lower>upper)
+    exit(0)
+    printnum(lower,upper);
+}
+
